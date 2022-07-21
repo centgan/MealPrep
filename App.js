1a074d59ef7015b5202d495e68f7995515ca475a
Centgan <ericonsmoke@gmail.com>
2022-07-20 21:10:48 -0400

    done food page react navigation ready

--- App.js
+++ App.js
@@ -6,6 +6,7 @@
  * @flow strict-local
  */
 
+
 import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
